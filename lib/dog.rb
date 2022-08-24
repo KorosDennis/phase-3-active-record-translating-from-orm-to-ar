b85dce99b2390c387835d5ab62a8e65c627f9c7d
korosdennis <deniptoo98@gmaiil.com>
2022-08-24 21:41:47 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,18 @@
-class Dog
+# require "pry"
+# require "active_record"
+     
+#     sql = <<-SQL
+#      CREATE TABLE IF NOT EXISTS dogs(
+#         id INTEGER PRIMARY KEY,
+#         name TEXT
+#      )
+#     SQL
+# ActiveRecord::Base.connection.execute(sql)
+
+# ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+class Dog < ActiveRecord::Base
+
 end
+
+# binding.pry
